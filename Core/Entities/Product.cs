08e5a23798d63a6cd1d5e5c5942cb0c5e34bd176
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2022-12-05 21:10:37 +0300

    add migration with other settings

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,8 +1,13 @@
 ﻿namespace Core.Entities
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+    public class Product : BaseEntity
+    {        
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; } = string.Empty;
+        public ProductType ProductType { get; set; } = new ProductType();
+        public int ProductTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; } = new ProductBrand();
+        public int ProductBrandId { get; set; }
     }
 }
