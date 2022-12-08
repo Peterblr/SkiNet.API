ed839605caa4107e393656191b25c0a92b891408
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2022-12-08 16:57:28 +0300

    rework ProductController for using Dto class

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Core.Entities;
+﻿using API.Dtos;
+using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,19 +18,46 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        public async Task<ActionResult<IEnumerable<ProductToReturnDto>>> GetProducts()
         {
             var products = await repository.GetAllProductsAsync();
 
-            return Ok(products);
+            var productsDto = products.Select(product => new ProductToReturnDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand?.Name,
+                ProductType = product.ProductType?.Name
+            }).ToList();
+
+            return Ok(productsDto);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var product = await repository.GetProductAsync(id);
 
-            return product == null ? NotFound() : product;
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var productDto = new ProductToReturnDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand?.Name,
+                ProductType = product.ProductType?.Name
+            };
+
+            return Ok(productDto);
         }
 
         [HttpGet("brands")]
