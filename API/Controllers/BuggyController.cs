325218fb934a178c51cde2c9b56ccef7150cca2f
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2022-12-09 11:24:30 +0300

    add controller with errors

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext context;
+
+        public BuggyController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var item = context.Products.Find(412);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+        
+        [HttpGet("servererror")]
+        public ActionResult GetSrverError()
+        {
+            var item = context.Products.Find(42);
+
+            var itemToReturn = item.ToString();
+
+            return Ok();
+        }
+        
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+        
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
