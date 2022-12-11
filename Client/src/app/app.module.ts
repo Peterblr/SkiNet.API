aae35bdf870c22bdbce90f8408c0a9767d9f2052
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2022-12-11 13:51:47 +0300

    add base settings to Angular project

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TooltipModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
