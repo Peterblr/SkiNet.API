2b63bdcbfe708eed818b727f12d1f277517e397c
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2022-12-13 11:51:32 +0300

    add interface for products

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +13,12 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Skinet';
   baseUrl = 'https://localhost:7064/api/products';
-  products: any[];
+  products: IProduct[];
 
   constructor(private http: HttpClient) {}
   
   ngOnInit(): void {
-    this.http.get(this.baseUrl).subscribe((res: any) => {
+    this.http.get(this.baseUrl).subscribe((res: IPagination) => {
       this.products = res.data;
     }, error => {
       console.log(error);
