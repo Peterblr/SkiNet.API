4bc712072772810c5fc75b020b3df63e20a8c050
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2023-05-10 19:03:48 +0300

    created test error component

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { ProductDatailsComponent } from './shop/product-datails/product-datails.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, FooterComponent],
+  declarations: [NavBarComponent, FooterComponent, TestErrorComponent],
   imports: [
     CommonModule,
     RouterModule
