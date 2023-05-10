9368f6c27f761ce45001bd2dab7d7241aae5dc8e
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2023-05-10 19:50:21 +0300

    created interceptor Http

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -4,11 +4,13 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, FooterComponent, TestErrorComponent],
+  declarations: [NavBarComponent, FooterComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
     RouterModule
